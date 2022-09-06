ea9890756ee5dc8635ac986629e23afe8ade00f3
Patrolin <patrik@balas.cz>
2022-09-06 12:37:40 +0200

    added breakpoint

--- src/App.js
+++ src/App.js
@@ -13,11 +13,18 @@ function App() {
   const pointerCoarse = useMediaQuery("(pointer: coarse)");
   const pointerFine = useMediaQuery("(pointer: fine)");
 
+  const breakPointXs = useMediaQuery("(min-width: 0px)");
+  const breakPointSm = useMediaQuery("(min-width: 600px)");
+  const breakPointMd = useMediaQuery("(min-width: 900px)");
+  const breakPointLg = useMediaQuery("(min-width: 1200px)");
+  const breakPointXl = useMediaQuery("(min-width: 1536px)");
+
   return (
     <div className="App">
       <header className="App-header">
         <Typography variant="h6">Hover: {printBool(hoverNone)}, {printBool(hoverHover)}</Typography>
         <Typography variant="h6">Pointer: {printBool(pointerNone)}, {printBool(pointerCoarse)}, {printBool(pointerFine)}</Typography>
+        <Typography variant="h6">Breakpoint: {[breakPointXs, breakPointSm, breakPointMd, breakPointLg, breakPointXl].map(printBool).join(", ")}</Typography>
       </header>
     </div>
   );
